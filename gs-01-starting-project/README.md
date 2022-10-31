80c6b1dff1d4bc2d3a2712636053519966667ff5
browndr <drew.brown@wwt.com>
2022-10-31 12:55:41 -0500

    Finish section 1.7

--- gs-01-starting-project/README.md
+++ gs-01-starting-project/README.md
@@ -1,5 +1,14 @@
 # Starting project
 
+## Section 1.5
+
+With vanilla JavaScript:
 How to we watch for the button to be clicked
 How to we get values from the input box?
 How do we update the shown list?
+
+## Section 1.7
+
+How can we converyt our JavaScript code to use Vue?
+How does our HTML need to change to suport Vue?
+How do we link objects together with Vue's custom attributes?
--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector('button'); // Get the button element from index.html
-const inputEl = document.querySelector('input'); // Get the input element from index.html
-const listEl = document.querySelector("ul"); // Get the ul element from index.html
+// const buttonEl = document.querySelector('button'); // Get the button element from index.html
+// const inputEl = document.querySelector('input'); // Get the input element from index.html
+// const listEl = document.querySelector("ul"); // Get the ul element from index.html
 
-function addGoal() {
-    const enteredValue = inputEl.value; // Get value from input
-    const listItemEl = document.createElement('li'); // Create list item element
-    listItemEl.textContent = enteredValue; // Update the list item element's text with the input value
-    listEl.appendChild(listItemEl); // Add new list item to list
-    inputEl.value = ''; // Clear the input value
-}
+// function addGoal() {
+//     const enteredValue = inputEl.value; // Get value from input
+//     const listItemEl = document.createElement('li'); // Create list item element
+//     listItemEl.textContent = enteredValue; // Update the list item element's text with the input value
+//     listEl.appendChild(listItemEl); // Add new list item to list
+//     inputEl.value = ''; // Clear the input value
+// }
 
-buttonEl.addEventListener('click', addGoal); // Tie clicking of the button to addGoal
+// buttonEl.addEventListener('click', addGoal); // Tie clicking of the button to addGoal
+
+Vue.createApp({
+    data() {
+        return {
+            goals: [], // List of all goals to display
+            enteredValue: '' // The data in the input box
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue); // Add value to displayed goals
+            this.enteredValue = ''; // Clear the input value
+        }
+    }
+}).mount('#app'); // mount() tells Vue where this code should take affect. Uses a CSS selector to match
