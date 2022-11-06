e0e9c97e28fb1842028d9797c5442fe36340e47a
browndr <drew.brown@wwt.com>
2022-11-06 09:51:19 -0600

    Use the native object event

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+            name: "",
+        };
+    },
+    methods: {
+        add(val) {
+            this.counter += val;
+        },
+        subtract(val) {
+            this.counter -= val;
+        },
+        clear() {
+            this.counter = 0;
+        },
+        setName(event) {
+            this.name = event.target.value;
+
+        },
+    },
+});
+
+app.mount('#events');
